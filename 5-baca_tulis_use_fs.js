83d53d2dd31f2876c27a4f49834a1af41d18fa1f
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-05-30 13:06:55 +0700

    penambahan index.html, css dan foto

--- 5-baca_tulis_use_fs.js
+++ 5-baca_tulis_use_fs.js
@@ -3,7 +3,7 @@
 // ==============================
 
 const http = require("http");
-
+const fs = require("fs");
 // http
 //   .createServer((req, res) => {
 //     res.writeHead(200, { "Content-Type": "text/html" });
@@ -16,18 +16,34 @@ const http = require("http");
 // MEMBACA FILE DI NODE JS
 // ========================
 
-const fs = require("fs");
-
 http
-  .createServer(function (req, res) {
-    fs.readFile("index.html", (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
+  .createServer((req, res) => {
+    fs.readFile("./index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
     });
   })
   .listen(8000);
+// http
+//   .createServer(function (req, res) {
+//     fs.readFile("index.html", (err, data) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       }
+//     });
+//   })
+//   .listen(8000);
+
+// =======================
+// MEMBUAT FILE DI NODEJS
+// =======================
+
+fs.appendFile("file1.txt", "Resky Bayu Andhika", function (err) {
+  if (err) throw err;
+  console.log("Saved!");
+});
